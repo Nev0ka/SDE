8d4c343cf615f2e654f1122bd09aabcc9198f62f
Nevoka <79937119+Nev0ka@users.noreply.github.com>
2023-01-31 11:14:33 +0100

    Add files via upload

---
+++ Obj Opg 3/Constructor/Person.cs
@@ -0,0 +1,17 @@
+﻿namespace Constructor
+{
+    internal class Person
+    {
+        public string name { get; set; }
+
+        public Person(string peopleName)
+        {
+            name = peopleName;
+        }
+
+        public override string ToString()
+        {
+            return $"Hello! My name is {name}";
+        }
+    }
+}
---
+++ Obj Opg 3/Constructor/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Constructor
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Person[] people = new Person[3];
+            Console.WriteLine("Input 3 names:");
+            string[] Names = Console.ReadLine().Split(' ', ',');
+            people[0] = new Person(Names[0]);
+            people[1] = new Person(Names[1]);
+            people[2] = new Person(Names[2]);
+
+            Console.WriteLine(people[0].ToString());
+            Console.WriteLine(people[1].ToString());
+            Console.WriteLine(people[2].ToString());
+            Console.Read();
+        }
+    }
+}
