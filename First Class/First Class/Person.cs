08379f20fd87968b72ca8afa67ddca4c5b440358
Nevoka <79937119+Nev0ka@users.noreply.github.com>
2023-01-31 10:47:41 +0100

    Add files via upload

---
+++ First Class/First Class/Person.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace First_Class
+{
+    internal class Person
+    {
+        public string name { get; set; }
+
+        public override string ToString()
+        {
+            return $"Hello! My name is {name}";
+        }
+    }
+}
---
+++ First Class/First Class/Program.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.SqlServer.Server;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace First_Class
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Person[] people = new Person[3];
+            Console.WriteLine("Input 3 random names:");
+            string[] Names = Console.ReadLine().Split(' ', ',');
+            people[0] = new Person() { name = Names[0] };
+            people[1] = new Person() { name = Names[1] };
+            people[2] = new Person() { name = Names[2] };
+
+            Console.WriteLine(people[0].ToString());
+            Console.WriteLine(people[1].ToString());
+            Console.WriteLine(people[2].ToString());
+            Console.Read();
+        }
+    }
+}
